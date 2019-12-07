6755f114193dabc05d40f7040e885ef911bb320c
melqueawq <melqueawq@gmail.com>
2019-12-08 00:22:41 +0900

    [fix] .envへのパスがおかしかったので修正

--- view/config.py
+++ view/config.py
@@ -2,9 +2,9 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')
+dotenv_path = join(dirname(__file__), '../.env')
 load_dotenv(dotenv_path)
 
-CONSUMER_KEY = os.getenv("CONSUMER_KEY")
-CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
-SECRET_KEY = os.getenv("SECRET_KEY")
+CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
+CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
+SECRET_KEY = os.environ.get("SECRET_KEY")
