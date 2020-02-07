b92ffc7a88d980f868c304992a2ac685a619c5a5
melqueawq <melqueawq@gmail.com>
2020-02-07 16:51:21 +0900

    [change] scss対応

--- view/_app.py
+++ view/_app.py
@@ -2,10 +2,13 @@
 # -*- coding:utf-8 -*-
 
 from flask import Flask
+from flask_scss import Scss
 
 from .config import SECRET_KEY
 
 app = Flask(__name__, template_folder='../templates',
             static_folder="../static")
+app.debug = True
+Scss(app, static_dir='static', asset_dir='assets')
 
 app.secret_key = SECRET_KEY
